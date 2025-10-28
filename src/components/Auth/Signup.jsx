244200bd22e24013b39f5bd8b7973fb567acefc1
Ridwan ✨ <107209973+JustRhidwan@users.noreply.github.com>
2025-10-28 23:32:32 +0100

    Update toast import path in Signup component

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import toast from "../../utils/toast";
+import toast from "/src/components/utils/toast";
 
 export default function Signup() {
   const navigate = useNavigate();
